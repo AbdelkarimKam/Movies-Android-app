71414c55d3745b8e52a371d3b36c97ba0cccf645
AbdelkarimKam <kammoun.karim2011@gmail.com>
2022-11-17 00:59:52 +0100

    correct load image in bindImage

--- app/src/main/java/com/yassir/challenge/movies/tools/adapter.kt
+++ app/src/main/java/com/yassir/challenge/movies/tools/adapter.kt
@@ -4,16 +4,15 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import coil.load
-import com.yassir.challenge.movies.tools.Constant.BASE_URL
 
 @BindingAdapter("imageUrl", "movieId")
 fun bindImage(imgView: ImageView, imgUrl: String?, movieId: Int) {
     imgUrl?.let {
-        imgUrl?.let {
-            // todo move this code to servirce to call with api_key Attention this code don't work
-            val imgUri =
-                imgUrl.toUri().buildUpon().scheme("$BASE_URL/movie/{$movieId}/images").build()
-            imgView.load(imgUri)
-        }
+        // todo move this code to servirce
+        val imgUri =
+            ("https://image.tmdb.org/t/p/w500/$it").toUri().buildUpon()
+                .scheme("https")
+                .build()
+        imgView.load(imgUri)
     }
 }
